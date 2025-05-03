ae724130b8eca70e86cf26e79b835a51a103bc71
Urs Adrian Florinel (Consulente) <adrian.urs00@gmail.com>
2025-05-03 04:02:33 +0300

    fixes navigator render on missing layout

--- dist/src/app-navigator/render.js
+++ dist/src/app-navigator/render.js
@@ -21,36 +21,31 @@ export const getRenderer = function ({ rootNode, componentsMap }) {
       </LayoutComponent>);
     }
     function renderNode(node, Navigator) {
-        if (node.layoutFile) {
+        if (!!node.layoutFile || !!node.children.length) {
             return renderNodeAsNavigator(node, Navigator);
         }
-        return renderNodeAsScreen(node, Navigator);
+        if (node.screenFile)
+            return renderNodeAsScreen(node, Navigator);
+        return null;
     }
     function renderNodeAsNavigator(node, Navigator) {
-        // If node has layoutFile, let's treat this as a "Layout"
-        // We can create a new stack or nested navigator
-        if (node.layoutFile) {
-            // TODO: Lazy load the layout component using require or dynamic import
-            const LayoutComponent = componentsMap[node.layoutFile];
-            const Routes = () => (<LayoutComponent>
+        // TODO: Lazy load the layout component using require or dynamic import
+        const LayoutComponent = (componentsMap[node.layoutFile] ?? DefaultLayout);
+        const Routes = () => (<LayoutComponent>
           {({ Navigator }) => {
-                    return (<>
-                {renderNodeAsScreen(node, Navigator)}
+                return (<>
+                {node.screenFile && renderNodeAsScreen(node, Navigator)}
                 {/* Render child routes (which might be pages or more layouts) */}
                 {node.children.map((child) => (<React.Fragment key={child.segment}>
                     {renderNode(child, Navigator)}
                   </React.Fragment>))}
               </>);
-                }}
+            }}
         </LayoutComponent>);
-            return Navigator ? (<Navigator.Screen name={node.segment} component={Routes}/>) : (<Routes />);
-        }
-        // If we reach here, maybe it’s a node with no layout and no page
-        // (e.g., an empty folder).
-        return null;
+        return Navigator ? (<Navigator.Screen name={node.segment} component={Routes}/>) : (<Routes />);
     }
     function renderNodeAsScreen(node, Navigator) {
-        if (!Navigator || !node.screenFile)
+        if (!Navigator)
             return null;
         return (<Navigator.Screen name={node.segment} component={componentsMap[node.screenFile]}/>);
     }
