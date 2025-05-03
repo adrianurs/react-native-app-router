f69de0fc4db32fde003e4ea9cbe83e74cbe58de1
Urs Adrian Florinel (Consulente) <adrian.urs00@gmail.com>
2025-05-03 04:00:07 +0300

    fixes navigator render on missing layout

--- src/app-navigator/render.tsx
+++ src/app-navigator/render.tsx
@@ -37,54 +37,49 @@ export const getRenderer: GetRenderer = function ({ rootNode, componentsMap }) {
   }
 
   function renderNode(node: Route, Navigator: Navigator | null) {
-    if (node.layoutFile) {
+    if (!!node.layoutFile || !!node.children.length) {
       return renderNodeAsNavigator(node, Navigator);
     }
 
-    return renderNodeAsScreen(node, Navigator);
-  }
+    if (node.screenFile) return renderNodeAsScreen(node, Navigator);
 
-  function renderNodeAsNavigator(node: Route, Navigator: Navigator | null) {
-    // If node has layoutFile, let's treat this as a "Layout"
-    // We can create a new stack or nested navigator
-    if (node.layoutFile) {
-      // TODO: Lazy load the layout component using require or dynamic import
-      const LayoutComponent = componentsMap[node.layoutFile] as FC<{
-        children: LayoutChildren;
-      }>;
-
-      const Routes = () => (
-        <LayoutComponent>
-          {({ Navigator }) => {
-            return (
-              <>
-                {renderNodeAsScreen(node, Navigator)}
-                {/* Render child routes (which might be pages or more layouts) */}
-                {node.children.map((child) => (
-                  <React.Fragment key={child.segment}>
-                    {renderNode(child, Navigator)}
-                  </React.Fragment>
-                ))}
-              </>
-            );
-          }}
-        </LayoutComponent>
-      );
-
-      return Navigator ? (
-        <Navigator.Screen name={node.segment} component={Routes} />
-      ) : (
-        <Routes />
-      );
-    }
-
-    // If we reach here, maybe it’s a node with no layout and no page
-    // (e.g., an empty folder).
     return null;
   }
 
+  function renderNodeAsNavigator(node: Route, Navigator: Navigator | null) {
+    // TODO: Lazy load the layout component using require or dynamic import
+    const LayoutComponent = (componentsMap[node.layoutFile!] ??
+      DefaultLayout) as FC<{
+      children: LayoutChildren;
+    }>;
+
+    const Routes = () => (
+      <LayoutComponent>
+        {({ Navigator }) => {
+          return (
+            <>
+              {node.screenFile && renderNodeAsScreen(node, Navigator)}
+              {/* Render child routes (which might be pages or more layouts) */}
+              {node.children.map((child) => (
+                <React.Fragment key={child.segment}>
+                  {renderNode(child, Navigator)}
+                </React.Fragment>
+              ))}
+            </>
+          );
+        }}
+      </LayoutComponent>
+    );
+
+    return Navigator ? (
+      <Navigator.Screen name={node.segment} component={Routes} />
+    ) : (
+      <Routes />
+    );
+  }
+
   function renderNodeAsScreen(node: Route, Navigator: Navigator | null) {
-    if (!Navigator || !node.screenFile) return null;
+    if (!Navigator) return null;
 
     return (
       <Navigator.Screen
